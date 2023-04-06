c003407ccfaf98c0e0231c73e1f3ebb16ee115c8
moracuatro <129897605+KhadaMora@users.noreply.github.com>
2023-04-06 15:49:01 -0600

    Creacion de Interfaz agregarPerfil.kt

--- app/src/main/java/com/example/medibox/agregarPerfil.kt
+++ app/src/main/java/com/example/medibox/agregarPerfil.kt
@@ -2,11 +2,24 @@ package com.example.medibox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.ArrayAdapter
 import android.widget.EditText
+import android.widget.Spinner
 
 class agregarPerfil : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agregar_perfil)
+
+        val spinnerdia: Spinner = findViewById(R.id.spinnerDia)
+
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.dias,
+            android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerdia.adapter = adapter
     }
 }
