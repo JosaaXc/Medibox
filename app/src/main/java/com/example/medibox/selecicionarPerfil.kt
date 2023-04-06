777a863156b2370ff6c1cb803950ba09bbd3c4f7
Alejandro <manuelmorm1@gmail.com>
2023-04-06 11:55:52 -0600

    Se cambio de nombre de clases

--- app/src/main/java/com/example/medibox/selecicionarPerfil.kt
+++ app/src/main/java/com/example/medibox/selecicionarPerfil.kt
@@ -3,12 +3,10 @@ package com.example.medibox
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class selecicionarPerfil : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_seleccionarperfil)
     }
-    class boton1(){
-        
-    }
+
 }
