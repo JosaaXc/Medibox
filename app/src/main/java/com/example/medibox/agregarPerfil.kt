e7641852569ff95a4fafbe0d9e8779f8b4610fd0
moracuatro <129897605+KhadaMora@users.noreply.github.com>
2023-04-06 18:31:48 -0600

    Se necesita arreglar datePicker

--- app/src/main/java/com/example/medibox/agregarPerfil.kt
+++ app/src/main/java/com/example/medibox/agregarPerfil.kt
@@ -14,6 +14,24 @@ class agregarPerfil : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agregar_perfil)
 
+        var selectedDate: String? = null
+
+        val editTextDate = findViewById<EditText>(R.id.editTextDate)
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(this,
+            { _, year, month, dayOfMonth ->
+                selectedDate = "$year-${month + 1}-$dayOfMonth"
+                editTextDate.setText(selectedDate)
+            }, year, month, dayOfMonth)
+
+        editTextDate.setOnClickListener {
+            datePickerDialog.show()
+        }
+
 
     }
 }
