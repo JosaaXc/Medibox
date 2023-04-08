4385acd3ea9fff0bf41fb5ee776f72883715eba5
JosaaXC <114438741+JosaaXc@users.noreply.github.com>
2023-04-08 11:27:36 -0600

    Responsivo agregado y vista de iniciarSesion

--- app/src/main/java/com/example/medibox/iniciarSesion.kt
+++ app/src/main/java/com/example/medibox/iniciarSesion.kt
@@ -1,7 +1,10 @@
 package com.example.medibox
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class iniciarSesion : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
