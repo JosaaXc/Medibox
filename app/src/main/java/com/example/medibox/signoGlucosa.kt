38b74b2fe4be54cd99b02551f8e566f12f852648
JosaaXC <114438741+JosaaXc@users.noreply.github.com>
2023-05-08 12:24:04 -0600

    pusheo de signos

--- app/src/main/java/com/example/medibox/signoGlucosa.kt
+++ app/src/main/java/com/example/medibox/signoGlucosa.kt
@@ -4,12 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
 import com.example.medibox.databinding.ActivitySignoGlucosaBinding
 import com.example.medibox.databinding.ActivitySignoRitmocBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class signoGlucosa : AppCompatActivity(), AdapterView.OnItemClickListener {
     private lateinit var binding: ActivitySignoGlucosaBinding
@@ -31,11 +31,26 @@ class signoGlucosa : AppCompatActivity(), AdapterView.OnItemClickListener {
     fun btnCancelar (view: View){
         val intent = Intent(this, bienvenida::class.java)
         startActivity(intent)
-        finish()
+        finishAffinity()
     }
 
     override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
         val item = parent?.getItemAtPosition(position).toString()
         Toast.makeText(this@signoGlucosa, item, Toast.LENGTH_SHORT).show()
+        val enviarSignoss = findViewById<TextView>(R.id.guardar)
+        enviarSignoss.setOnClickListener{
+            enviarSignos(item)
+        }
+    }
+    fun enviarSignos(item: String){
+        val databasereference: DatabaseReference = FirebaseDatabase.getInstance().getReference("/")
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        val uid = currentUser?.uid.toString()
+
+        databasereference.child("Signos").child(uid).child("glucosa").push().setValue(item)
+        Toast.makeText(this@signoGlucosa, item+" \nFue agregado con éxito", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this, bienvenida::class.java)
+        startActivity(intent)
+        finishAffinity()
     }
 }
--- app/src/main/java/com/example/medibox/signoOxigenacion.kt
+++ app/src/main/java/com/example/medibox/signoOxigenacion.kt
@@ -1,13 +1,18 @@
 package com.example.medibox
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import android.widget.Toast
 import com.example.medibox.databinding.ActivitySignoOxigenacionBinding
 import com.example.medibox.databinding.ActivitySignoRitmocBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class signoOxigenacion : AppCompatActivity(), AdapterView.OnItemClickListener {
 
@@ -27,10 +32,29 @@ class signoOxigenacion : AppCompatActivity(), AdapterView.OnItemClickListener {
             onItemClickListener = this@signoOxigenacion
         }
     }
+    fun btnCancelar (view: View){
+        val intent = Intent(this, bienvenida::class.java)
+        startActivity(intent)
+        finishAffinity()
+    }
 
     override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
         //aqui obtenemos el elemento seleccionado y lo guardamos en item
         val item = parent?.getItemAtPosition(position).toString()
-        Toast.makeText(this@signoOxigenacion, item, Toast.LENGTH_SHORT).show()
+        val enviarSignoss = findViewById<TextView>(R.id.guardar)
+        enviarSignoss.setOnClickListener{
+            enviarSignos(item)
+        }
+    }
+    fun enviarSignos(item: String){
+        val databasereference: DatabaseReference = FirebaseDatabase.getInstance().getReference("/")
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        val uid = currentUser?.uid.toString()
+
+        databasereference.child("Signos").child(uid).child("oxigenacion").push().setValue(item)
+        Toast.makeText(this@signoOxigenacion, item+" \nFue agregado con éxito", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this, bienvenida::class.java)
+        startActivity(intent)
+        finishAffinity()
     }
 }
